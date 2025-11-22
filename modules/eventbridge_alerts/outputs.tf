2785c202ad2964a752f1eb0dfb5a1f40ae6e15b8
Charles-bucher <quietopscb@gmail.com>
2025-11-22 01:51:08 -0500

    Normalize .tf filenames, move Terraform files to root

--- modules/eventbridge_alerts/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "rule_id" {
-  description = "ID of the EventBridge rule"
-  value       = aws_cloudwatch_event_rule.guardduty_findings.id
-}
-
-output "rule_arn" {
-  description = "ARN of the EventBridge rule"
-  value       = aws_cloudwatch_event_rule.guardduty_findings.arn
-}
--- modules/sns_alerts/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "topic_arn" {
-  description = "ARN of the SNS topic"
-  value       = aws_sns_topic.guardduty_alerts.arn
-}
-
-output "topic_name" {
-  description = "Name of the SNS topic"
-  value       = aws_sns_topic.guardduty_alerts.name
-}
